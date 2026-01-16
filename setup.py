31dc698ab888578c65463627df9b9d55fc203fec
93chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-01-16 10:00:16 +0100

    Updated inspector capacity and version for new upload

--- setup.py
+++ setup.py
@@ -40,9 +40,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyedahelper',
-    version='1.0.6',
-    author='Chidiebere Christopher',
-    author_email='chidiebere@example.com',
+    version='1.0.7',
+    author='Chidiebere V. Christopher',
+    author_email='vchidiebere.vc@gmail.com',
     description="An interactive cheat sheet, AI-powered guide for exploratory data analysis (EDA), and tools for data visualization, cleaning and feature engineering.",
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
